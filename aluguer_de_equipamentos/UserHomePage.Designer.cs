5a6fe854745499b2e17466e05c8a8026308a80a8
diogopires17 <63168110+diogopires17@users.noreply.github.com>
2024-04-19 15:43:40 +0100

    user form advances

--- aluguer_de_equipamentos/UserHomePage.Designer.cs
+++ aluguer_de_equipamentos/UserHomePage.Designer.cs
@@ -30,6 +30,14 @@
         {
             this.UserEquipmentList = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDisponibilidade = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLocalizacao = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // UserEquipmentList
@@ -50,18 +58,92 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Alugar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(414, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(417, 49);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(310, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.Location = new System.Drawing.Point(417, 118);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(310, 20);
+            this.txtCategoria.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(414, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // txtDisponibilidade
+            // 
+            this.txtDisponibilidade.Location = new System.Drawing.Point(417, 184);
+            this.txtDisponibilidade.Name = "txtDisponibilidade";
+            this.txtDisponibilidade.Size = new System.Drawing.Size(310, 20);
+            this.txtDisponibilidade.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(414, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // txtLocalizacao
+            // 
+            this.txtLocalizacao.Location = new System.Drawing.Point(417, 257);
+            this.txtLocalizacao.Name = "txtLocalizacao";
+            this.txtLocalizacao.Size = new System.Drawing.Size(310, 20);
+            this.txtLocalizacao.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(414, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
             // 
             // UserHomePage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(987, 568);
+            this.Controls.Add(this.txtLocalizacao);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDisponibilidade);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCategoria);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.UserEquipmentList);
             this.Name = "UserHomePage";
             this.Text = "UserHomePage";
             this.Load += new System.EventHandler(this.UserHomePage_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,5 +151,13 @@
 
         private System.Windows.Forms.ListBox UserEquipmentList;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDisponibilidade;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLocalizacao;
+        private System.Windows.Forms.Label label4;
     }
 }
--- aluguer_de_equipamentos/UserHomePage.cs
+++ aluguer_de_equipamentos/UserHomePage.cs
@@ -14,24 +14,25 @@ namespace aluguer_de_equipamentos
 {
     public partial class UserHomePage : Form
     {
+        private SqlConnection cn;
+        private int  equipamnetoSelecionado = 1 ;
+        private List<Equipamento> equipamentos = new List<Equipamento>(); 
+
         public UserHomePage()
         {
             InitializeComponent();
+            showEquipamento();
         }
         private void UserHomePage_Load(object sender, EventArgs e)
         {
-
             cn = getSGBDConnection();
             loadEquipmentsToolStripMenuItem_Click(sender, e);
-
         }
 
         private SqlConnection getSGBDConnection()
         {
             return new SqlConnection("data source= DIOGOPIRES\\SQLEXPRESS;integrated security=true;initial catalog=aluguer_equipamentos");
         }
-
-        private SqlConnection cn;
 
         private bool verifySGBDConnection()
         {
@@ -45,14 +46,15 @@ namespace aluguer_de_equipamentos
         }
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            equipamnetoSelecionado = UserEquipmentList.SelectedIndex;
+            showEquipamento();
         }
         private void loadEquipmentsToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if (!verifySGBDConnection())
                 return;
 
-            SqlCommand cmd = new SqlCommand("SELECT * FROM Equipamento", cn);
+            SqlCommand cmd = new SqlCommand("SELECT E.*, L.cidade FROM Equipamento E INNER JOIN localizacao L ON E.id_localizacao = L.id_localizacao", cn);
             SqlDataReader reader = cmd.ExecuteReader();
             UserEquipmentList.Items.Clear();
 
@@ -62,13 +64,49 @@ namespace aluguer_de_equipamentos
                 E.Nome = (string)reader["nome"];
                 E.Disponivel = (bool)reader["disponivel"];
                 E.Categoria = (string)reader["categoria"];
-                UserEquipmentList.Items.Add($"{E.Nome},  {E.Categoria} - {(E.Disponivel ? "Disponivel" : "Não disponível")}");
-
+                E.IdLocalizacao = (int)reader["id_localizacao"];
+                string cidade = (string)reader["cidade"]; 
+                equipamentos.Add(E);
+                UserEquipmentList.Items.Add($"{E.Nome},  {E.Categoria}, {cidade}  - {(E.Disponivel ? "Disponivel" : "Não disponível")}");
             }
 
             cn.Close();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+
+        // OUTRAS FUNÇÕES
+        private void  showEquipamento()
+        {
+            if(UserEquipmentList.Items.Count == 0 | UserEquipmentList.SelectedIndex < 0)
+            {
+                return;
+            }
+            Equipamento E = equipamentos[equipamnetoSelecionado];
+            txtNome.Text = E.Nome;
+            txtCategoria.Text = E.Categoria;
+            txtLocalizacao.Text = E.IdLocalizacao.ToString();
+            txtDisponibilidade.Text = E.Disponivel ? "Disponivel" : "Não disponivel";
+
+            
+
+            desativaCampos();
+
+        }
+
+        private void desativaCampos()
+        {
+            txtNome.ReadOnly = true;
+            txtCategoria.ReadOnly = true;
+            txtLocalizacao.ReadOnly = true;
+            txtDisponibilidade.ReadOnly = true;
+        }
 
     }
 }
